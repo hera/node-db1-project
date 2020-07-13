9696533725b40b6ca1e964218e79d4fe660b1df2
hera <a.heraimenka@gmail.com>
2020-07-13 13:39:30 -0700

    Get an account by id

--- api/accounts/accountsRouter.js
+++ api/accounts/accountsRouter.js
@@ -3,10 +3,35 @@ const accountsDb = require("../../data/accountsDb");
 
 const router = express.Router();
 
+
+// Get all accounts
+
 router.get("/", (req, res) => {
     accountsDb.get()
         .then(accounts => {
             res.status(200).json(accounts);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: "Server error. Could not get all accounts.",
+                description: error
+            });
+        });
+});
+
+
+// Get an account by id
+
+router.get("/:id", (req, res) => {
+    accountsDb.getById(req.params.id)
+        .then(accounts => {
+            if (accounts.length) {
+                res.status(200).json(accounts);
+            } else {
+                res.status(404).json({
+                    error: `Could not find an account with id ${req.params.id}`
+                });
+            }
         })
         .catch(error => {
             res.status(500).json({
--- data/accountsDb.js
+++ data/accountsDb.js
@@ -1,11 +1,18 @@
 const db = require("./dbConfig");
 
+const TABLE_NAME = "accounts";
+
 module.exports = {
-    get
+    get,
+    getById
 };
 
-// 
 
 function get () {
-    return db("accounts");
+    return db(TABLE_NAME);
+}
+
+
+function getById (id) {
+    return db(TABLE_NAME).where({id});
 }
